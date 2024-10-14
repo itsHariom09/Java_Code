49f3082a2962258291c04cc2ea2a103124224467
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2024-10-15 00:14:52 +0530

    Pattern with Sout

---
+++ PatternWithSOP.java
@@ -0,0 +1,15 @@
+public class PatternWithSOP {
+    public static void main(String[] args) {
+        /*WAP to print the pattern with System.out.println() 
+         ****
+         ***
+         **
+         *
+        
+        */
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
