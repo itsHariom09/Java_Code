104ba853aa623362094e3425a65685356ae00dc6
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2024-10-15 00:21:09 +0530

    Variables in JAVA

---
+++ VariablesInJava.java
@@ -0,0 +1,11 @@
+public class VariablesInJava {
+    public static void main(String[] args) {
+        // Variables in Java
+        // Variables are used to store data in a program.
+        // Variables are declared using the data type of the variable.
+        // For example, int, float, char, boolean etc.
+        int a=10;
+        int b=20;
+        System.out.println("SUM: "+(a+b));
+    }
+}
