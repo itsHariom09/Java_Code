f61e22e2c25d097120bcdbf85dfbd961a4316bd5
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2024-10-15 23:52:36 +0530

    Product of two number

---
+++ ProductOfTwoNumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ProductOfTwoNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a=sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b=sc.nextInt();
+        int product=a*b;
+        System.out.println("Product of two numbers is: "+product);
+    }
+}
