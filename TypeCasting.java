0e4695fa0fe0432bf8cf162b35b395c0f047c1fb
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2024-10-16 00:07:58 +0530

    Type Casting

---
+++ TypeCasting.java
@@ -0,0 +1,10 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        // Explicit Casting
+        //Java not  allow implicit casting of primitive types
+        //we forcely  convert one type to another using explicit casting
+        float a=10.66f;
+        int b=(int)a;//Explicit Casting
+        System.out.println("Value of b is "+b);
+    }
+}
