05542aab197b2cef3ad2c732ad9dd95481cda959
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2025-01-31 23:41:10 +0530

    star

--- Pattern Code in JAVA/PrintStarPattern1.java
+++ Pattern Code in JAVA/PrintStarPattern1.java
@@ -6,10 +6,11 @@ public class PrintStarPattern1 {
                     * * *
                     * * * *
          */
-        for(int i=0;i<4;i++){
-            for(int j=0;j<=i;j++){
-                System.out.println("* ");
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
             }
+            System.out.println();
         }
     }
 }
