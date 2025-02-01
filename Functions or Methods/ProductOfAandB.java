38f4a8275414f3f8b8cf01f7e646061f6bebf9f5
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2025-02-01 10:56:31 +0530

    Product of a nad b

---
+++ Functions or Methods/ProductOfAandB.java
@@ -0,0 +1,9 @@
+public class ProductOfAandB {
+    public static int mult(int a,int b){
+        return a*b;
+    }
+    public static void main(String[] args) {
+        int prod=mult(5,3);
+        System.out.println("Product of 5 and 3 is: "+prod);
+    }
+}
