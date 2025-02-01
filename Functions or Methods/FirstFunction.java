ff2f028349c373929da01386e6ff5525315b7666
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2025-02-01 10:20:05 +0530

    first function

---
+++ Functions or Methods/FirstFunction.java
@@ -0,0 +1,8 @@
+public class FirstFunction {
+    public static void hello(){
+        System.out.println("Hello, World!");
+    }
+    public static void main(String[] args) {
+        hello();
+    }
+}
