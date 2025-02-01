09d24655fdbcc1137df34fcef2edc4980b41b3f9
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2025-02-01 10:27:54 +0530

    Parameter Function

---
+++ Functions or Methods/ParameterFunction.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ParameterFunction {
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a=sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b=sc.nextInt();
+        System.out.println("Sum of "+a+" and "+b+" is "+add(a,b));
+    }
+}
