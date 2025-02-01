d13d19aebc1e29cf6b74bd28a18c8c85e0396d81
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2025-02-01 14:31:33 +0530

    change

--- Functions or Methods/ConvertDecimalToBinary.java
+++ Functions or Methods/ConvertDecimalToBinary.java
@@ -4,7 +4,7 @@ public class ConvertDecimalToBinary {
         int binary=0;
         while(n>0){
             int lastnum=n%2;
-            binary=binary+lastnum*(int)Math.pow(10,pow);
+            binary=binary+(lastnum*(int)Math.pow(10,pow));
             n=n/2;
             pow++;
         }
