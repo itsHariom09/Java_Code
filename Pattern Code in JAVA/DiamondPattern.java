1ff3b2636e3ffbc043264af41bf97d01ef7a3d23
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2025-02-05 00:57:54 +0530

    Diamond Pattern

---
+++ Pattern Code in JAVA/DiamondPattern.java
@@ -0,0 +1,38 @@
+public class DiamondPattern {
+    /*
+     * Java Program to print the Diamond Pattern
+                     *
+                    ***
+                   *****
+                  *******
+                  *******
+                   *****
+                    ***
+                     *
+     */
+    public static void diamondPattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        diamondPattern(4);
+    }
+    
+}
