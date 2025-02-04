29e18f7da94f3cd4cb52ecf461b115564a3e5e11
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2025-02-05 00:47:17 +0530

    Hollow Rhombus

---
+++ Pattern Code in JAVA/HollowRhombus.java
@@ -0,0 +1,29 @@
+public class HollowRhombus {
+    /*
+     * Java Program to print Hollow Rhombus Star Pattern
+                    *****
+                   *   *
+                  *   *
+                 *   *
+                *****
+     */
+    public static void hollowRhombus(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=n;j++){
+                if(i==1 || i==n || j==1 || j==n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        hollowRhombus(5);
+    }
+}
