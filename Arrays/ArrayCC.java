25beaf1db9c92aa549868c282d6dd7c1589f1e3e
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2025-02-12 23:26:17 +0530

    Array

--- Arrays/ArrayCC.java
+++ Arrays/ArrayCC.java
@@ -1,7 +1,19 @@
 import java.util.*;
 
-public class arrayCC {
+public class ArrayCC {
     public static void main(String[] args) {
-        
+        int num[]=new int[10];
+        num[0]=99;
+        num[1]=65;
+        num[2]=54;
+        System.out.println("Num 1:"+num[0]);
+        System.out.println("Num 2:"+num[1]);
+        System.out.println("Num 3:"+num[2]);
+        //Update Array
+        num[1]=76;
+        System.out.println("Updated Num 2:"+num[1]);
+        //Print Length
+        System.out.println("Length of Array:"+num.length);
+
     }
 }
