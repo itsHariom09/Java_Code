f497c740a0ae56843ec0201bba30b2911ff22d85
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2025-02-15 23:09:26 +0530

    Max subarray Sum

--- Array2/MaxSubarraySum.java
+++ Array2/MaxSubarraySum.java
@@ -2,7 +2,21 @@ import java.util.*;
 public class MaxSubarraySum {
     //Broute force approach
     public static void MaxSubarraySum(int arr[]) {
-        
+        int currentSum=0;
+        int maxSum=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            for (int j=0;j<arr.length;j++){
+                currentSum=0;
+                for (int k=i;k<=j;k++){
+                    currentSum+=arr[k];
+                }
+                if(maxSum<currentSum) {
+                    maxSum = currentSum;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("Max Sum: "+maxSum);
 
     }
     public static void main(String[] args) {
