60288360a8560de486f725318276762af78dcf1e
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2025-02-18 23:50:55 +0530

    Trapped water problem

---
+++ Array2/TrappedWaterProblem.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class TrappedWaterProblem {
+    public static int trappedWater(int height[]) {
+        int n=height.length;
+        //For left Max Bar
+        int leftMax[]= new int[n];
+        leftMax[0]=height[0];
+        for (int i=1;i<n;i++){
+            leftMax[i]=Math.max(height[i],leftMax[i-1]);
+        }
+        //For Right Max Bar
+        int rightMax[]= new int[n];
+        rightMax[n-1]=height[n-1];
+        for (int i=n-2;i>=0;i--){
+            rightMax[i]=Math.max(height[i],rightMax[i+1]);
+        }
+        int trappedWaterinBar=0;
+        for (int i=0;i<n;i++){
+            int waterLevel=Math.min(leftMax[i],rightMax[i]);
+            trappedWaterinBar=trappedWaterinBar+waterLevel-height[i];
+        }
+        return trappedWaterinBar;
+    }
+    public static void main(String[] args) {
+        int height[]={4,2,0,6,3,2,5};
+        System.out.println(trappedWater(height));
+    }
+}
