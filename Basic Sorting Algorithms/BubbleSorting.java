9c61fff479963ca6b667a454e0085fa1378fe659
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2025-02-19 22:53:59 +0530

    Bubble sort

---
+++ Basic Sorting Algorithms/BubbleSorting.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class BubbleSorting {
+    public static void bubbleSort(int arr[]) {
+        
+    }
+
+    public static void main(String[] args) {
+        int[] arr={5,4,1,3,2};
+        bubbleSort(arr);
+    }
+}
