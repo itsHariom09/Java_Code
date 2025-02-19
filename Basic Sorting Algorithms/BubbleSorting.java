801a38c4fc7e7443418e81c118f3aec3ac459afa
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2025-02-19 23:20:10 +0530

    Bubble Sort

--- Basic Sorting Algorithms/BubbleSorting.java
+++ Basic Sorting Algorithms/BubbleSorting.java
@@ -3,7 +3,7 @@ public class BubbleSorting {
     public static void bubbleSort(int arr[]) {
         for(int turn=0;turn<arr.length-1;turn++){
             int swap=0;
-            for(int j=0;j<arr.length-1;j++){
+            for(int j=0;j<arr.length-1-turn;j++){
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
