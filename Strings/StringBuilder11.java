b2497aaa0e08f86ed82f4aa639576fa8ea870de5
HariomPrajapati09 <hariomprajapatiwork@gmail.com>
2025-02-24 22:27:56 +0530

    StringBuilder

---
+++ Strings/StringBuilder11.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class StringBuilder11 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" World");
+        System.out.println(sb);
+
+    }
+}
