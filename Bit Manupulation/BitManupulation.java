9d747dd64918d8af999375da9925d35e6309eed5
Hariom <hariomprajapatiwork@gmail.com>
2025-04-05 17:13:28 +0530

    Bit Manuplulation

--- Bit Manupulation/BitManupulation.java
+++ Bit Manupulation/BitManupulation.java
@@ -1,20 +1,33 @@
 public class BitManupulation {
-    public static void main(String[] args) {
-        int a=5,b=6;
-        //Bitwise AND
-        System.err.println(a&b);
-        //System.out.println(a|b); //Bitwise OR
-        //System.out.println(a^b); //Bitwise XOR
-        //System.out.println(~a); //Bitwise NOT
-        //System.out.println(a<<1); //Left Shift
-        //System.out.println(a>>1); //Right Shift
-        //System.out.println(a>>>1); //Unsigned Right Shift
-        //System.out.println(a&(~(1<<2))); //Clear bit at position 2
-        //System.out.println(a|(1<<2)); //Set bit at position 2
-        //System.out.println(a^(1<<2)); //Toggle bit at position 2
-        //System.out.println((a&1)==1); //Check if last bit is set
-        //System.out.println((a&1)==0); //Check if last bit is not set
-        //System.out.println((a&1)==1 ? "Odd" : "Even"); //Check if odd or even
-        //System.out.println((a&1)==0 ? "Even" : "Odd"); //Check if even or odd
-    }
+        public static void main(String[] args) {
+            int a=5,b=6;
+            //Bitwise AND
+            System.out.println(a&b);
+            //Bitwise OR
+            System.out.println(a|b);
+            //Bitwise XOR
+            System.out.println(a^b); 
+            //Bitwise NOT
+            System.out.println(~a); 
+            //Left Shift
+            System.out.println(a<<1); 
+            //Right Shift
+            System.out.println(a>>1); 
+            //Unsigned Right Shift
+            System.out.println(a>>>1); 
+            //Clear bit at position 2
+            System.out.println(a&(~(1<<2))); 
+            //Set bit at position 2
+            System.out.println(a|(1<<2)); 
+            //Toggle bit at position 2
+            System.out.println(a^(1<<2)); 
+            //Check if last bit is set
+            System.out.println((a&1)==1); 
+            //Check if last bit is not set
+            System.out.println((a&1)==0); 
+            //Check if odd or even
+            System.out.println((a&1)==1 ? "Odd" : "Even"); 
+            //Check if even or odd
+            System.out.println((a&1)==0 ? "Even" : "Odd"); 
+        }    
 }
